a9fbd99349f4c00c4065fa5f184107ba680cc773
Niko Leskinen <niko.leskinen@fmi.fi>
2022-06-13 16:52:09 +0300

    Setup configs

---
+++ src/cloudnet_submit/cli.py
@@ -0,0 +1,6 @@
+def cli() -> None:
+    print("Hi from cli!!")
+
+
+if __name__ == "__main__":
+    cli()
---
+++ tests/test_cloudnet_submit.py
@@ -0,0 +1,6 @@
+from cloudnet_submit.cli import cli
+
+
+def test_submit():
+    cli()
+    assert True is True
